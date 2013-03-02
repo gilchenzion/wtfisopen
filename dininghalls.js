64bd4d345824c79df0dad92fb05822913323762d
Gil Chen-Zion <gc2466@columbia.edu>
2013-03-02 07:09:17 -0500

    Latest Version of Dining Hall App

--- dininghalls.js
+++ dininghalls.js
@@ -4,9 +4,9 @@
 		var min = date.getMinutes();
 		
 		function check(){
-			setStatus(".Fer_status", checkFer());
-			setStatus(".JohnJay_status",checkJohnJay());
-			setStatus(".JJs_status", checkJJs());
+			setStatus("Fer", checkFer());
+			setStatus("JohnJay",checkJohnJay());
+			setStatus("JJs", checkJJs());
 		}
 		
 		function checkFer(){
@@ -15,7 +15,12 @@
 			{
 				default:
 					if(hour >= 8 && hour < 20){
-						status = "Open";
+						if( hour < 11 )
+							status = "BR";
+						else if (hour < 16 )
+							status = "LU";
+						else
+							status = "DN";
 					} else {
 						status = "Closed";
 					}
@@ -26,10 +31,10 @@
 						if(hour == 10 && min < 30){
 							status = "Closed";
 						} else {
-							status = "Open";
+							status = "BN";
 						}
 					} else if(hour >= 17 && hour < 20){
-						status = "Open";
+						status = "DN";
 					} else {
 						status = "Closed";
 					}		
@@ -42,14 +47,23 @@
 			switch (day)
 			{
 				default:
+					
 					if((hour >= 11 && hour < 14) || (hour >= 17 && hour < 20)){
-						status = "Open";
+						if (hour < 14){
+							status = "BN";
+						} else {
+							status = "DN";
+						}
 					} else {
 						status = "Closed";
 					}
 				case 0:
+					
 					if((hour >= 10 && hour < 14) || (hour >= 17 && hour < 20)){
-						status = "Open";
+						if (hour < 14)
+							status = "BN";
+						else
+							status = "DN";
 					} else {
 						status = "Closed";
 					}
@@ -67,31 +81,37 @@
 			{
 				default:
 					if(hour >= 20 || hour < 1){
-						status = "Open";
+						status = "LN";
 					} else {
 						status = "Closed";
 					}
 				case 0:
 					if(hour >= 12 || hour < 1){
-						status = "Open";
+						if (hour < 20)
+							status = "BN";
+						else
+							status = "LN";
 					} else {
 						status = "Closed";
 					}
 				case 1:
 					if(hour >= 12 || hour < 1){
-						status = "Open";
+						if (hour < 20)
+							status = "BN";
+						else
+							status = "LN";
 					} else {
 						status = "Closed";
 					}
 				case 5:
 					if(hour >= 12 && hour < 20){
-						status = "Open";
+						status = "BN";
 					} else {
 						status = "Closed";
 					}
 				case 6:
 					if(hour >= 12 && hour < 20){
-						status = "Open";
+						status = "BN";
 					} else {
 						status = "Closed";
 					}	
@@ -100,5 +120,27 @@
 		}
 		
 		function setStatus(hall, status){
-			$(hall).append(status);
+			if(status != "Closed") {
+				$("." + hall + "_status").append("Open");
+				$("." + hall + "_status").css('color', '#76EE00');
+			} else {
+				$("." + hall + "_status").append(status);
+				$("." + hall + "_status").css('color', 'red');
+			}
+		}
+		
+		function getFullName(status){
+			if ( status == "DN") {
+				return "Dinner";
+			} else if (status == "BN") {
+				return "Brunch";
+			} else if (status == "BR") {
+				return "Breakfast";
+			} else if (status == "LU") {
+				return "Lunch";
+			} else if (status == "LN") {
+				return "Late Night";
+			} else {
+				return "Closed";
+			}
 		}
