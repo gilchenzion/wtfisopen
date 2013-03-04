2f3e2fd9388e96169bf282853608749261f527dc
Gil Chen-Zion <gc2466@columbia.edu>
2013-03-04 11:34:13 -0500

    Switch issue fix
    fix the switch issue. put in break line

--- dininghalls.js
+++ dininghalls.js
@@ -13,19 +13,10 @@
 			var status;
 			switch (day)
 			{
-				default:
-					if(hour >= 8 && hour < 20){
-						if( hour < 11 )
-							status = "BR";
-						else if (hour < 16 )
-							status = "LU";
-						else
-							status = "DN";
-					} else {
-						status = "Closed";
-					}
+				
 				case 0:
 					status = "Closed";
+					break;
 				case 6:
 					if(hour >= 10 && hour < 14){
 						if(hour == 10 && min < 30){
@@ -37,7 +28,20 @@
 						status = "DN";
 					} else {
 						status = "Closed";
-					}		
+					}	
+					break;
+				default:
+					if(hour >= 8 && hour < 20){
+						if( hour < 11 ) {
+							status = "BR";
+						} else if (hour < 16 ) {
+							status = "LU";
+						} else{
+							status = "DN";
+						}
+					} else {
+						status = "Closed";
+					}	
 			}
 			return status;
 		}
@@ -46,8 +50,23 @@
 			var status;
 			switch (day)
 			{
+				case 0:
+					if((hour >= 10 && hour < 14) || (hour >= 17 && hour < 20)){
+						if (hour < 14)
+							status = "BN";
+						else
+							status = "DN";
+					} else {
+						status = "Closed";
+					}
+					break;
+				case 5:
+					status = "Closed";
+					break;
+				case 6:
+					status = "Closed";	
+					break;
 				default:
-					
 					if((hour >= 11 && hour < 14) || (hour >= 17 && hour < 20)){
 						if (hour < 14){
 							status = "BN";
@@ -57,20 +76,6 @@
 					} else {
 						status = "Closed";
 					}
-				case 0:
-					
-					if((hour >= 10 && hour < 14) || (hour >= 17 && hour < 20)){
-						if (hour < 14)
-							status = "BN";
-						else
-							status = "DN";
-					} else {
-						status = "Closed";
-					}
-				case 5:
-					status = "Closed";
-				case 6:
-					status = "Closed";	
 			}
 			return status;
 		}
@@ -79,12 +84,7 @@
 			var status;
 			switch (day)
 			{
-				default:
-					if(hour >= 20 || hour < 1){
-						status = "LN";
-					} else {
-						status = "Closed";
-					}
+				
 				case 0:
 					if(hour >= 12 || hour < 1){
 						if (hour < 20)
@@ -94,6 +94,7 @@
 					} else {
 						status = "Closed";
 					}
+					break;
 				case 1:
 					if(hour >= 12 || hour < 1){
 						if (hour < 20)
@@ -103,6 +104,7 @@
 					} else {
 						status = "Closed";
 					}
+					break;
 				case 5:
 					if(hour >= 12 && hour < 20){
 						status = "BN";
@@ -114,7 +116,14 @@
 						status = "BN";
 					} else {
 						status = "Closed";
-					}	
+					}
+					break;	
+				default:
+					if(hour >= 20 || hour < 1){
+						status = "LN";
+					} else {
+						status = "Closed";
+					}
 			}
 			return status;
 		}
